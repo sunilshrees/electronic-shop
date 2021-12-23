336d8b003e1c62ed0c54fa5a5bdb21f0911a6a5f
sunilshrees <sunilshrees371@gmail.com>
2021-12-23 14:37:44 +0545

    used react form

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -3,38 +3,72 @@ import { useForm } from 'react-hook-form';
 import '../styles/checkout.css';
 
 function CheckoutForm() {
-    // const { register, handleSubmit, errors } = useForm({});
+    const { register, handleSubmit } = useForm({});
 
-    // const onSubmit = (data) => {
-    //     console.log(data);
-    // };
+    const onSubmit = ({ name, email }) => {
+        alert(`Name: ${name}, Email: ${email}`);
+    };
     return (
-        <div className='hero'>
-            <div className='checkout__container'>
-                <div className='checkout__form'>
-                    <div className='heading'>Payment Details</div>
-                    <div className='sub-heading'>
-                        Complete your purchase by providing your details.
-                    </div>
-
-                    <form className='inputForm'>
-                        {/* <label className='fieldText'>Full Name</label> */}
-                        <input type='text' placeholder='Name' name='name' />
-
-                        {/* <label className='fieldText'>Email address</label> */}
-                        <input type='text' placeholder='Email' name='email' />
-
-                        {/* <label className='fieldText'>Delivery Address</label> */}
-                        <input type='text' placeholder='Address' />
-
-                        {/* <label className='fieldText'>Phone Number</label> */}
-                        <input type='text' placeholder='Phone no.' />
-
-                        <button className='payment-btn' type='submit'>
-                            Payment now
-                        </button>
-                    </form>
+        <div className='checkout__container'>
+            <div className='checkout__form'>
+                <div className='heading'>Payment Details</div>
+                <div className='sub-heading'>
+                    Complete your purchase by providing your details.
                 </div>
+
+                <form className='inputForm' onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor='name' className='fieldText'>
+                        Full Name
+                    </label>
+                    <input
+                        type='text'
+                        {...register('name', { required: 'Required' })}
+                        autoComplete='off'
+                        placeholder='Name'
+                        name='name'
+                        id='name'
+                    />
+
+                    <label htmlFor='email' className='fieldText'>
+                        Email address
+                    </label>
+                    <input
+                        type='text'
+                        autoComplete='off'
+                        placeholder='Email'
+                        name='email'
+                        id='email'
+                        {...register('email', { required: 'Required' })}
+                    />
+
+                    <label htmlFor='address' className='fieldText'>
+                        Delivery Address
+                    </label>
+                    <input
+                        type='text'
+                        autoComplete='off'
+                        placeholder='Address'
+                        name='address'
+                        id='address'
+                        {...register('address', { required: 'Required' })}
+                    />
+
+                    <label className='fieldText' htmlFor='phone'>
+                        Phone Number
+                    </label>
+                    <input
+                        type='tel'
+                        autoComplete='off'
+                        placeholder='Phone no.'
+                        name='phone'
+                        id='phone'
+                        {...register('phone', { required: 'Required' })}
+                    />
+
+                    <button className='payment-btn' type='submit'>
+                        Payment now
+                    </button>
+                </form>
             </div>
         </div>
     );
